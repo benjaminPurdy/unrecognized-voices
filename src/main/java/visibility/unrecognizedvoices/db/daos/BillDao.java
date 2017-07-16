fbeb84fc4d5e253a3deab60c66749b6ba944c42a
benjaminpurdy <benjaminpurdy@hotmail.com>
2017-07-16 04:30:31 -0400

    worked like a charm

--- src/main/java/visibility/unrecognizedvoices/db/daos/BillDao.java
+++ src/main/java/visibility/unrecognizedvoices/db/daos/BillDao.java
@@ -12,7 +12,6 @@ import java.util.List;
  */
 @RegisterMapper(BillMapper.class)
 public interface BillDao {
-	//test
 
     @SqlQuery("select * from Bill where id = :id")
     Bill findBill(@BindBean Bill bill);
